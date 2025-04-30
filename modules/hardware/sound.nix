44e5771a75605626f3f2b43be73b289f758e674d
Dreamgineer <me@dgnr.us>
2025-04-30 18:19:57 -0400

    again

--- modules/hardware/sound.nix
+++ modules/hardware/sound.nix
@@ -16,7 +16,7 @@
     wireplumber.extraConfig."99-stop-microphone-auto-adjust" = {
       "access.rules" = [
         {
-          matches = [ { application.process.binary = "vesktop"; } ];
+          matches = [ { application.process.binary = "electron"; } ];
           actions = {
             update-props = {
               default_permissions = "rx";
