c96081af9652b1ebbeb87c5d83b888ace67bf136
Dreamgineer <me@dgnr.us>
2025-05-12 08:34:21 -0400

    Fix again

--- modules/home/hypr/services.nix
+++ modules/home/hypr/services.nix
@@ -28,6 +28,13 @@
           };
         };
       };
+      output = {
+        backlight = {
+          name = "eDP-1";
+          path = "/sys/class/backlight/intel_backlight";
+          capturer = "wayland";
+        };
+      };
     };
   };
 }
