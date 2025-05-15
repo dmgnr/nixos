b4a980e1785c4932c17ef2792d12f609f5f21a53
Dreamgineer <me@dgnr.us>
2025-05-14 23:32:07 -0400

    adjust brightness keybindings to change by 2% instead of 10%

--- modules/home/hypr/default.nix
+++ modules/home/hypr/default.nix
@@ -167,8 +167,8 @@
       ",XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
       ",XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
       ",XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
-      ",XF86MonBrightnessUp, exec, brightnessctl s 10%+;hyprctl notify 1 2000 0 \"Brightness increased\""
-      ",XF86MonBrightnessDown, exec, brightnessctl s 10%-;hyprctl notify 1 2000 0 \"Brightness reduced\""
+      ",XF86MonBrightnessUp, exec, brightnessctl s 2%+;hyprctl notify 1 2000 0 \"Brightness increased\""
+      ",XF86MonBrightnessDown, exec, brightnessctl s 2%-;hyprctl notify 1 2000 0 \"Brightness reduced\""
     ];
     bindl = [
       # Requires playerctl
