e22ff86ff7893bd69f24f53634830b025572ef60
Dreamgineer <me@dgnr.us>
2025-05-14 23:32:17 -0400

    remove auto-upgrades configuration from system settings

--- modules/software/system.nix
+++ modules/software/system.nix
@@ -15,16 +15,7 @@
       ];
     };
   };
-
-  # Enable auto-upgrades.
-  system.autoUpgrade = {
-    enable = true;
-    # Run daily
-    dates = "daily";
-    # Build the new config and make it the default, but don't switch yet.  This will be picked up on reboot.  This helps
-    # prevent issues with OpenSnitch configs not well matching the state of the system.
-    operation = "boot";
-  };
+  
   services.system76-scheduler.enable = true;
   hardware.system76.power-daemon.enable = true;
 
