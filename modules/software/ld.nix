bdb34aeb5a85d39d282e71a22b631c9407f37410
Dreamgineer <me@dgnr.us>
2025-05-28 10:30:30 -0400

    Fixes

--- modules/software/ld.nix
+++ modules/software/ld.nix
@@ -3,22 +3,16 @@
 {
   # Let's be more pragmatic and try to run binaries sometimes
   # at the cost of sweeping bugs under the rug.
-  programs = {
-    nix-ld = {
-      enable = true;
-      # put whatever libraries you think you might need
-      # nix-ld includes a strong sane-default as well
-      # in addition to these
-      libraries = with pkgs; [
-        stdenv.cc.cc.lib
-        zlib
-      ];
-    };
+  programs.nix-ld = {
+    enable = true;
+    # put whatever libraries you think you might need
+    # nix-ld includes a strong sane-default as well
+    # in addition to these
+    libraries = with pkgs; [
+      stdenv.cc.cc.lib
+      zlib
+    ];
   };
 
-  services = {
-    envfs = {
-      enable = true;
-    };
-  };
+  services.envfs.enable = true;
 }
