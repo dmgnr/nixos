8db0c4af710bcd547aa1cd0c10a44f1e6943c1a2
Dreamgineer <me@dgnr.us>
2025-05-27 23:07:27 -0400

    Remove wluma service configuration

--- modules/home/hypr/services.nix
+++ modules/home/hypr/services.nix
@@ -16,28 +16,4 @@
       ];
     };
   };
-  services.wluma = {
-    enable = true;
-    settings = {
-      als = {
-        time = {
-          thresholds = {
-            "0" = "latenight";
-            "7" = "day";
-            "20" = "night";
-            "23" = "latenight";
-          };
-        };
-      };
-      output = {
-        backlight = [
-          {
-            name = "eDP-1";
-            path = "/sys/class/backlight/intel_backlight";
-            capturer = "wayland";
-          }
-        ];
-      };
-    };
-  };
 }
