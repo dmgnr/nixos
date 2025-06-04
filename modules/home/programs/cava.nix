840f92854b4cb0035ff29ebdac3f06640910660f
installer <installer@gmail.com>
2025-06-04 12:26:35 -0400

    hyprwinwrap

--- modules/home/programs/cava.nix
+++ modules/home/programs/cava.nix
@@ -1,9 +1,14 @@
+{ pkgs, ... }:
 {
   programs.cava = {
     enable = true;
     settings = {
-      autosens = 1;
-      channels = "mono";
+      general.autosens = 1;
+      output.channels = "mono";
     };
   };
+  wayland.windowManager.hyprland.plugins = [
+    pkgs.hyprlandPlugins.hyprwinwrap
+  ];
+  wayland.windowManager.hyprland.settings.plugin.hyprwinwrap.class = "cava";
 }
