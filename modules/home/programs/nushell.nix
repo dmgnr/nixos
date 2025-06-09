53676c1454473a64d5b34209dbf0cfb98d67822d
installer <installer@gmail.com>
2025-06-09 14:31:21 -0400

    Add direnv

--- modules/home/programs/nushell.nix
+++ modules/home/programs/nushell.nix
@@ -24,4 +24,11 @@
 
   # Carapace
   programs.carapace.enable = true;
+
+  # Direnv
+  programs.direnv = {
+    enable = true;
+    enableBashIntegration = true; # see note on other shells below
+    nix-direnv.enable = true;
+  };
 }
