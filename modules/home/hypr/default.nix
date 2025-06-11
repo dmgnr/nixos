97b563ea6facd4fd879b8f1a663ffa363be62ba3
installer <installer@gmail.com>
2025-06-11 19:38:23 -0400

    Instant full screen record

--- modules/home/hypr/default.nix
+++ modules/home/hypr/default.nix
@@ -28,6 +28,8 @@
       "pkill qalc; hyprctl dispatch exec \"[size 350 400; move 100%-350 100%-420; pin; float; stayfocused; dimaround; animation slide bottom]kitty qalc\"";
     "$record" =
       "bash -c 'pkill wf-recorder || (hyprctl notify 1 2000 0 \"Recording started\" && wf-recorder -f recording.mp4 -y -g \"$(slurp)\" && wl-copy -t video/mp4 < recording.mp4 && hyprctl notify 1 2000 0 \"Recording finished\")'";
+    "$recordfull" =
+      "bash -c 'pkill wf-recorder || (hyprctl notify 1 2000 0 \"Recording started\" && wf-recorder -f recording.mp4 -y && wl-copy -t video/mp4 < recording.mp4 && hyprctl notify 1 2000 0 \"Recording finished\")'";
     "$togglewb" =
       "pkill -SIGUSR1 waybar";
 
@@ -195,6 +197,7 @@
       ", print, exec, hyprshot -m region --clipboard-only -z -s"
       "SHIFT, print, exec, hyprshot -m output -m active --clipboard-only -s"
       "ALT, print, exec, $record"
+      "ALT SHIFT, print, exec, $recordfull"
     ];
     windowrule = [
       # Ignore maximize requests from apps. You'll probably like this.
