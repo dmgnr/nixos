8103e7eacbb841937c5b19f3b7fa0414a543ca39
installer <installer@gmail.com>
2025-06-17 12:51:04 -0400

    Enable wireplumber

--- modules/hardware/sound.nix
+++ modules/hardware/sound.nix
@@ -13,6 +13,7 @@
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
     #media-session.enable = true;
+    wireplumber.enable = true;
     wireplumber.extraConfig."99-stop-microphone-auto-adjust" = {
       "access.rules" = [
         {
