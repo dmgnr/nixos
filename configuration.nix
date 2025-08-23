86c89784d9051e4e8af72499e9c2c1f3ff490856
installer <installer@gmail.com>
2025-08-24 00:05:59 +0700

    a

--- configuration.nix
+++ configuration.nix
@@ -2,7 +2,12 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 {
   imports = [
     # Include the results of the hardware scan.
@@ -93,8 +98,9 @@
     sbctl
     firefox
     comma
-    
+
     tpm2-tss
+    (callPackage ./thorium/thorium.nix { })
   ];
 
   # Let's be more pragmatic and try to run binaries sometimes
@@ -124,7 +130,9 @@
   programs.xwayland.enable = true;
 
   services.openvpn.servers = {
-    ctf  = { config = '' config /etc/nixos/minimal/ctf.conf ''; };
+    ctf = {
+      config = ''config /etc/nixos/ctf.conf '';
+    };
   };
 
   boot = {
@@ -171,139 +179,139 @@
 
     supportedFilesystems = [ "ntfs" ];
   };
-  
-    users.users.dgnr = {
-      isNormalUser = true;
-      description = "Dreamgineer";
-      extraGroups = [
-        "networkmanager"
-        "plugdev"
-        "wheel"
-        "docker"
-      ];
-      packages = with pkgs; [
-        git
-        vesktop
-        fastfetch
-        nixd
-        nixfmt-rfc-style
-        gh
-        spotube
-        inshellisense
-        gearlever
-        appimage-run
-        nvtopPackages.full
-        scrcpy
-        #  thunderbird
-      ];
+
+  users.users.dgnr = {
+    isNormalUser = true;
+    description = "Dreamgineer";
+    extraGroups = [
+      "networkmanager"
+      "plugdev"
+      "wheel"
+      "docker"
+    ];
+    packages = with pkgs; [
+      git
+      vesktop
+      fastfetch
+      nixd
+      nixfmt-rfc-style
+      gh
+      spotube
+      inshellisense
+      gearlever
+      appimage-run
+      nvtopPackages.full
+      scrcpy
+      #  thunderbird
+    ];
+  };
+
+  security.sudo.wheelNeedsPassword = false;
+  security.pam.services.hyprlock = { };
+
+  nix.settings.trusted-users = [
+    "root"
+    "dgnr"
+  ];
+  networking.hostName = "nyx"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+  # networking.wireless.iwd.enable = true;
+  # networking.networkmanager.wifi.backend = "iwd";
+
+  # Networking
+  services.tailscale.enable = true;
+  # "100.100.100.100" "45.90.28.164" "45.90.30.164"
+  networking.nameservers = [
+    "45.90.28.164"
+    "1.1.1.1"
+    "45.90.30.164"
+    "8.8.8.8"
+  ];
+  networking.search = [
+    "tail71b97a.ts.net"
+    "dgnr.us"
+  ];
+  networking.firewall.enable = false;
+
+  hardware.bluetooth.enable = true;
+  hardware.bluetooth.powerOnBoot = true;
+  services.blueman.enable = true;
+
+  services.avahi = {
+    nssmdns4 = true;
+    enable = true;
+    ipv4 = true;
+    ipv6 = true;
+    publish = {
+      enable = true;
+      addresses = true;
+      workstation = true;
     };
-    
-      security.sudo.wheelNeedsPassword = false;
-      security.pam.services.hyprlock = { };
-    
-      nix.settings.trusted-users = [
-        "root"
-        "dgnr"
-      ];
-        networking.hostName = "nyx"; # Define your hostname.
-        # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-      
-        # Enable networking
-        networking.networkmanager.enable = true;
-        # networking.wireless.iwd.enable = true;
-        # networking.networkmanager.wifi.backend = "iwd";
-      
-        # Networking
-        services.tailscale.enable = true;
-        # "100.100.100.100" "45.90.28.164" "45.90.30.164"
-        networking.nameservers = [
-          "45.90.28.164"
-          "1.1.1.1"
-          "45.90.30.164"
-          "8.8.8.8"
-        ];
-        networking.search = [
-          "tail71b97a.ts.net"
-          "dgnr.us"
-        ];
-        networking.firewall.enable = false;
-      
-        hardware.bluetooth.enable = true;
-        hardware.bluetooth.powerOnBoot = true;
-        services.blueman.enable = true;
-      
-        services.avahi = {
-          nssmdns4 = true;
-          enable = true;
-          ipv4 = true;
-          ipv6 = true;
-          publish = {
-            enable = true;
-            addresses = true;
-            workstation = true;
-          };
-        };
-        
-          # Enable OpenGL
-          hardware.graphics = {
-            enable = true;
-            enable32Bit = true;
-          };
-        
-          # Load nvidia driver for Xorg and Wayland
-          services.xserver.videoDrivers = [ "nvidia" ];
-        
-          hardware.nvidia = {
-            prime = {
-              reverseSync.enable = true;
-              # Enable if using an external GPU
-              allowExternalGpu = false;
-        
-              # Make sure to use the correct Bus ID values for your system!
-              intelBusId = "PCI:1:0:0";
-              nvidiaBusId = "PCI:0:2:0";
-              # amdgpuBusId = "PCI:54:0:0"; For AMD GPU
-            };
-        
-            # Modesetting is required.
-            modesetting.enable = true;
-        
-            # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-            # Enable this if you have graphical corruption issues or application crashes after waking
-            # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
-            # of just the bare essentials.
-            powerManagement.enable = true;
-        
-            # Fine-grained power management. Turns off GPU when not in use.
-            # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-            powerManagement.finegrained = false;
-        
-            # Use the NVidia open source kernel module (not to be confused with the
-            # independent third-party "nouveau" open source driver).
-            # Support is limited to the Turing and later architectures. Full list of
-            # supported GPUs is at:
-            # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
-            # Only available from driver 515.43.04+
-            open = false;
-        
-            # Enable the Nvidia settings menu,
-            # accessible via `nvidia-settings`.
-            nvidiaSettings = true;
-        
-            # Optionally, you may need to select the appropriate driver version for your specific GPU.
-            package = config.boot.kernelPackages.nvidiaPackages.stable;
-          };
-          
-            zramSwap.enable = true;
-            swapDevices = [
-              {
-                device = "/var/lib/swapfile";
-                size = 8 * 1024;
-                priority = 1;
-              }
-            ];
-          
-            hardware.ksm.enable = true;
+  };
+
+  # Enable OpenGL
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
+
+  # Load nvidia driver for Xorg and Wayland
+  services.xserver.videoDrivers = [ "nvidia" ];
+
+  hardware.nvidia = {
+    prime = {
+      reverseSync.enable = true;
+      # Enable if using an external GPU
+      allowExternalGpu = false;
+
+      # Make sure to use the correct Bus ID values for your system!
+      intelBusId = "PCI:1:0:0";
+      nvidiaBusId = "PCI:0:2:0";
+      # amdgpuBusId = "PCI:54:0:0"; For AMD GPU
+    };
+
+    # Modesetting is required.
+    modesetting.enable = true;
+
+    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+    # Enable this if you have graphical corruption issues or application crashes after waking
+    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
+    # of just the bare essentials.
+    powerManagement.enable = true;
+
+    # Fine-grained power management. Turns off GPU when not in use.
+    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+    powerManagement.finegrained = false;
+
+    # Use the NVidia open source kernel module (not to be confused with the
+    # independent third-party "nouveau" open source driver).
+    # Support is limited to the Turing and later architectures. Full list of
+    # supported GPUs is at:
+    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
+    # Only available from driver 515.43.04+
+    open = false;
+
+    # Enable the Nvidia settings menu,
+    # accessible via `nvidia-settings`.
+    nvidiaSettings = true;
+
+    # Optionally, you may need to select the appropriate driver version for your specific GPU.
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+
+  zramSwap.enable = true;
+  swapDevices = [
+    {
+      device = "/var/lib/swapfile";
+      size = 8 * 1024;
+      priority = 1;
+    }
+  ];
+
+  hardware.ksm.enable = true;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
